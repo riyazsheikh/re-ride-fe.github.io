9494b8b582aed704668d10b426c80a3a66fc0d39
riyazsheikh <riyajsheikh@gmail.com>
2017-04-07 20:41:22 +0530

    replacing with pratiksha’s index n js file

--- ReRideBLEpromises.js
+++ ReRideBLEpromises.js
@@ -2,52 +2,46 @@
  * Created by sokoler on 04/08/16.
  */
 
+
+
 const serviceUuid = "user_data";
-const characteristicUuid = "age";
-const characteristicUuid1 = "weight";
+const characteristicWeight = "weight";
+const characteristicAge = "age";
 const deviceName = "ReRide";
 
 var myCharacteristic;
-var myCharacteristic1;
-var gauge;
-var gauge1;
+var gauge_w;
+var gauge_a;
 
-function initGauge()
-{
-    gauge = new JustGage({
-        id: "gauge",
+function initGauge() {
+    gauge_w = new JustGage({
+        id: "gauge_w",
         value: 0,
         min: 0,
         max: 255,
-        title: "Visitors"
+        title: "Weight"
     });
-}
-
-function initGauge1()
-{
-    gauge1 = new JustGage({
-        id: "gauge1",
+    gauge_a = new JustGage({
+        id: "gauge_a",
         value: 0,
         min: 0,
         max: 255,
-        title: "Visitors1"
+        title: "Age"
     });
 }
 
-function setGauge(currentValue)
-{
-    gauge.refresh(currentValue);
+function setGauge_a(currentValue) {
+    gauge_a.refresh(currentValue);
 }
 
-function setGauge1(currentValue)
-{
-    gauge1.refresh(currentValue);
+function setGauge_w(currentValue) {
+    gauge_w.refresh(currentValue);
 }
 
 function onStartButtonClick() {
 
     log('Requesting Bluetooth Device...');
-    navigator.bluetooth.requestDevice({filters: [{name: deviceName}], optionalServices:[serviceUuid]})
+    navigator.bluetooth.requestDevice({ filters: [{ name: deviceName }], optionalServices: [serviceUuid] })
         .then(device => {
             log('Connecting to GATT Server...');
             return device.gatt.connect();
@@ -58,28 +52,24 @@ function onStartButtonClick() {
         })
         .then(service => {
             log('Getting Characteristic...');
-            //tst = service.getCharacteristic(characteristicUuid);
-            return service.getCharacteristic(characteristicUuid);
-        })
-        then(service => {
-            log('Getting Characteristic...');
-            //tst = service.getCharacteristic(characteristicUuid);
-            return service.getCharacteristic(characteristicUuid1);
+            var weight = service.getCharacteristic(characteristicWeight);
+            var age = service.getCharacteristic(characteristicAge);
+            return { weight: weight, age: age };
         })
         .then(characteristic => {
             myCharacteristic = characteristic;
-            return myCharacteristic.startNotifications().then(_ => {
+            var p1 = myCharacteristic.weight.startNotifications().then(_ => {
                 log('> Notifications started');
-                myCharacteristic.addEventListener('characteristicvaluechanged',
-                    handleNotifications);
+                myCharacteristic.weight.addEventListener('characteristicweightchanged',
+                    handleWeightNotifications);
             });
-            .then(characteristic => {
-                myCharacteristic1 = characteristic;
-                return myCharacteristic1.startNotifications().then(_ => {
-                    log('> Notifications started');
-                    myCharacteristic1.addEventListener('characteristicvaluechanged',
-                        handleNotifications1);
-                });
+
+            var p2 = myCharacteristic.age.startNotifications().then(_ => {
+                log('> Notifications started');
+                myCharacteristic.age.addEventListener('characteristicagechanged',
+                    handleAgeNotifications);
+            });
+
         })
         .catch(error => {
             log('Argh! ' + error);
@@ -91,50 +81,39 @@ function onStopButtonClick() {
         myCharacteristic.stopNotifications()
             .then(_ => {
                 log('> Notifications stopped');
-                myCharacteristic.removeEventListener('characteristicvaluechanged',
-                    handleNotifications);
-            })
-            .catch(error => {
-                log('Argh! Sensor 1! ' + error);
-            });
-    }
+                myCharacteristic.removeEventListener('characteristicweightchanged',
+                    handleWeightNotifications);
 
-    if (myCharacteristic1) {
-        myCharacteristic1.stopNotifications()
-            .then(_ => {
-                log('> Notifications stopped');
-                myCharacteristic1.removeEventListener('characteristicvaluechanged',
-                    handleNotifications1);
+                myCharacteristic.removeEventListener('characteristicagechanged',
+                    handleAgeNotifications);
             })
             .catch(error => {
-                log('Argh! Sensor 2!' + error);
+                log('Argh! ' + error);
             });
     }
 }
 
-function handleNotifications(event) {
+function handleWeightNotifications(event) {
     let value = event.target.value;
- /*   let b = value.getUint8(0).toString();
-
-    // now do stuff with the data received !
-    log('> ' + b);
- */
+    /*   let b = value.getUint8(0).toString();
+   
+       // now do stuff with the data received !
+       log('> ' + b);
+    */
     let x = value.getUint8(0);
-    setGauge(x);
+    setGauge_w(x);
 
 }
 
-function handleNotifications1(event) {
+function handleAgeNotifications(event) {
     let value = event.target.value;
- /*   let b = value.getUint8(0).toString();
-
-    // now do stuff with the data received !
-    log('> ' + b);
- */
+    /*   let b = value.getUint8(0).toString();
+   
+       // now do stuff with the data received !
+       log('> ' + b);
+    */
     let x = value.getUint8(0);
-    setGauge1(x);
+    setGauge_a(x);
 
 }
-
 initGauge();
-initGauge1();
