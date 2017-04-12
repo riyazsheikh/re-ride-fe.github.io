18c0e4ddb95dfe8cf75d3489e17ca11e701c4666
riyazsheikh <riyajsheikh@gmail.com>
2017-04-12 16:53:07 +0530

    Update ReRideBLEpromises.js
    two different characteristics for the age and weight

--- ReRideBLEpromises.js
+++ ReRideBLEpromises.js
@@ -10,7 +10,8 @@ const characteristicWeight = "weight";
 const characteristicAge = "age";
 const deviceName = "ReRide";
 
-var myCharacteristic;
+var myAgeCharacteristic;
+var myWeightCharacteristic;
 var gauge_w;
 var gauge_a;
 
@@ -57,25 +58,28 @@ function onStartButtonClick() {
             var age = service.getCharacteristic(characteristicAge);
             return { weight: weight, age: age };
         })
-        .then(characteristic => {
-            myCharacteristic = characteristic;
-            var p1 = myCharacteristic.startNotifications()
+        .then(characteristic => 
+         {
+            myWeightCharacteristic = characteristicWeight;
+            myAgeCharacteristic = characteristicAge;
+        
+            var p1 = myWeightCharacteristic.startNotifications()
             .then(_ => {
                 log('> Notifications started');
-                myCharacteristic.addEventListener('characteristicweightchanged',
+                myWeightCharacteristic.addEventListener('characteristicweightchanged',
                     handleWeightNotifications);
             });
 
-            var p2 = myCharacteristic.startNotifications().then(_ => {
+            var p2 = myAgeCharacteristic.startNotifications().then(_ => {
                 log('> Notifications started');
-                myCharacteristic.addEventListener('characteristicagechanged',
+                myAgeCharacteristic.addEventListener('characteristicagechanged',
                     handleAgeNotifications);
             });
 
         })
         .catch(error => {
             log('Argh! ' + error);
-        });
+        });    
 }
 
 function onStopButtonClick() {
